4df31737c4636fcc8bab6bafeccdd09cb12d7c8f
bilal khalid <sbkhalid201@gmail.com>
2017-03-29 16:14:58 -0400

    controller methods donezo

--- bison_bookshop/app/controllers/books_controller.rb
+++ bison_bookshop/app/controllers/books_controller.rb
@@ -1,13 +1,23 @@
 class BooksController < ApplicationController
   def index
+    if !user_sign_in?
+      redirect_to new_user_session_path
+    else
+    @books = Book.all #returning a book that is searched
+  end
   end
 
   def show
+    @book = Book.find(params[:id]) #individual book and the reviews attached
+    @review = Review.new()
+    @review.book_id=@book.id
   end
 
   def edit
   end
 
   def new
+    @book = Book.new()
+
   end
 end
