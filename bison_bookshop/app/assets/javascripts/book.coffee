35839e712a952e79b893d334c8ece0ddde5ce72b
Jason Andrada <jpa323@nyu.edu>
2017-03-30 11:02:22 -0400

    ajax calls are live

---
+++ bison_bookshop/app/assets/javascripts/book.coffee
@@ -0,0 +1,7 @@
+$-> #document.ready
+#(selector).on("ajax:success", (e, data, status, xhr) ->
+#stuff you want to do)
+#Again, Sabrina saved our lives.
+#form_for has to have remote: true
+  # $('example form').on("ajax:success", (e, data, status, xhr) ->
+  #   )
--- bison_bookshop/app/controllers/books_controller.rb
+++ bison_bookshop/app/controllers/books_controller.rb
@@ -4,21 +4,29 @@ class BooksController < ApplicationController
     if !user_signed_in?
       redirect_to new_user_session_path
     else
-    @books = Book.all #returning a book that is searched
+    @books = GoogleBooks.search(title)
+    #setting up an AJAX call in the backend.
+      response_to do |format|
+        format.html
+        format.js {render json: @books, content_type: 'text/json'}
+      end
     end
   end
-
   def show
     @review = Review.new
     @review.book_id=@book.id
   end
-
   def edit
   end
-
+  def update
+    if @book.update(book_params)
+      redirect_back fallback_location: :root
+    else
+      redirect_to :book
+    end
+  end
   def new
     @book = Book.new
-
   end
   def create
     @book = Book.new(book_params)
@@ -28,7 +36,6 @@ class BooksController < ApplicationController
       redirect_to :book
     end
   end
-
   def destroy
     if @book.destroy
       redirect_to :root
