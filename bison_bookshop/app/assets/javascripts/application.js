b54ec2980c417cd875b0f52d8919d77d68bf5696
caroline guntert <carolineguntert@gmail.com>
2017-03-31 16:25:19 -0400

    getting there

--- bison_bookshop/app/assets/javascripts/application.js
+++ bison_bookshop/app/assets/javascripts/application.js
@@ -16,4 +16,63 @@
 //= require_tree .
 $(document).ready(() => {
   console.log('YO BOOKS!');
+
+
+
+//login shit $('.form').find('input, textarea').on('keyup blur focus', function (e) {
+
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight');
+			} else {
+		    label.removeClass('highlight');
+			}
+    } else if (e.type === 'focus') {
+
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight');
+			}
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
 });
+
+$('.tab a').on('click', function (e) {
+
+  e.preventDefault();
+
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+
+  $(target).fadeIn(600);
+
+});///end of log in form
+
+
+
+
+
+
+
+
+
+
+
+
+
+});//end of doc ready
