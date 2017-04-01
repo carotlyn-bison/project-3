141660924fb1f7de01ccb82343459a39267df68f
Jason Andrada <jpa323@nyu.edu>
2017-04-01 18:54:38 -0400

    edit functionality online

--- bison_bookshop/config/routes.rb
+++ bison_bookshop/config/routes.rb
@@ -12,12 +12,12 @@ Rails.application.routes.draw do
   post "/books", to: "books#create" #no helper bc we're posting
   delete "/books/:id", to: "books#destroy" #no helper bc we're deleting
 
-  get "reviews/index", to: "reviews#index" #, as: :reviews
   get "reviews/new", to: "reviews#new", as: :new_review
+  get "reviews/:id", to: "reviews#show", as: :review
   get "reviews/:id/edit", to: "reviews#edit", as: :edit_review
   post "/reviews", to: "reviews#create" #no helper bc we're posting
-  patch "reviews/:id", to: "reviews#update" #no helper bc we're patching
-  delete "reviews/:id", to: "reviews#destroy" #no helper bc we're deleting
+  patch "/reviews/:id", to: "reviews#update" #no helper bc we're patching
+  delete "/reviews/:id", to: "reviews#destroy" #no helper bc we're deleting
 
   get "/users/:id", to: "users#show", as: :user # user_path
 
