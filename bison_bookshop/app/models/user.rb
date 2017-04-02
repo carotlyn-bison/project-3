8b4f877dc787bad0c19b75a8c5960897dd3ba1b6
Jason Andrada <jpa323@nyu.edu>
2017-04-02 16:29:34 -0400

    followers live in terminal

--- bison_bookshop/app/models/user.rb
+++ bison_bookshop/app/models/user.rb
@@ -7,5 +7,16 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   has_and_belongs_to_many :books
   has_many :reviews
+  #following/follower functionality
+  #https://www.railstutorial.org/book/following_users
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  def follow(other_user)
+    following << other_user
+  end
+  def unfollow(other_user)
+    following.delete other_user
+  end
+  def following?(other_user)
+    following.include?(other_user)
 end
