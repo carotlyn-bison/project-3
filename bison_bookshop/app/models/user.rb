a907b888329d51984bde4844225e5ae5aa6952b0
Jason Andrada <jpa323@nyu.edu>
2017-04-02 17:02:03 -0400

    still working through

--- bison_bookshop/app/models/user.rb
+++ bison_bookshop/app/models/user.rb
@@ -10,13 +10,16 @@ class User < ApplicationRecord
   #following/follower functionality
   #https://www.railstutorial.org/book/following_users
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
   def follow(other_user)
     following << other_user
   end
   def unfollow(other_user)
-    following.delete other_user
+    following.delete(other_user)
   end
   def following?(other_user)
     following.include?(other_user)
+  end
 end
