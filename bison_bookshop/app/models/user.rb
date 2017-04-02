b27dc675acb72720815579598ca7c25d582a6aa6
Jason Andrada <jpa323@nyu.edu>
2017-04-01 22:50:44 -0400

    started work on voting mech

--- bison_bookshop/app/models/user.rb
+++ bison_bookshop/app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :email, uniqueness: true
+  acts_as_voter
   has_and_belongs_to_many :books
   has_many :reviews
 end
