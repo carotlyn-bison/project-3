6496b4be76d752632c30bb0ba20f771ac3336cb3
Jason Andrada <jpa323@nyu.edu>
2017-04-01 23:18:33 -0400

    upvotes and downvotes are LIVE

--- bison_bookshop/app/models/user.rb
+++ bison_bookshop/app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :email, uniqueness: true
-  acts_as_voter
   has_and_belongs_to_many :books
   has_many :reviews
 end
